43ff35c378ec1b2a350b39a9ae81e670b145d972
Ethan Saintot <ethan.saintot@epitech.eu>
2025-06-24 11:54:21 +0200

    add python version

---
+++ juste_prix.py
@@ -0,0 +1,15 @@
+import random
+
+prix_random = random.randint(1, 100)
+print(prix_random) #comment this line
+
+while 1:
+    prix_user = int(input("Quel prix"))
+
+    if (prix_user == prix_random):
+        print("Bien joué")
+        break
+    elif (prix_user < prix_random):
+        print("C'est plus")
+    else:
+        print("C'est moins")
